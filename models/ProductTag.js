4457acf0413c6093307840422dbe835c9230cd8e
Mohamed Lazrek <mohamed.lazrekb@gmail.com>
2021-10-29 00:00:50 -0400

    bug fixes

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,26 +6,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // id: {
-    //   type: DataTypes.INTEGER,
-    //   allowNull: false,
-    //   primaryKey: true,
-    //   autoIncrement: true,
-    // },
-    // product_id: {
-    //   type: DataTypes.INTEGER,
-    //   references: {
-    //     model: "product",
-    //     key: "id",
-    //   },
-    // },
-    // tag_id: {
-    //   type: DataTypes.INTEGER,
-    //   references: {
-    //     model: "tag",
-    //     key: "id",
-    //   },
-    // },
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
